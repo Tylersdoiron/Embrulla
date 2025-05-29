b2d5161af5b629e90ddcba2b1e4edf2f66ac99e3
Tylersdoiron <stickyfingersdigital@gmail.com>
2025-05-29 19:39:34 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import EmbrullaRoutes from './EmbrullaRoutes';
+
+function App() {
+  return (
+    <Router>
+      <EmbrullaRoutes />
+    </Router>
+  );
+}
+
+export default App;
